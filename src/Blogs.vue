5726e77ceaa6ea53ec99307b0ef97849688168cc
Nick <nick@itprojmgt.com>
2019-03-03 15:17:59 -0700

    add filter

---
+++ src/Blogs.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="blogs">
+    <h2>Blogs</h2>
+    <div class="blog" v-for="post in posts" :key="post.id">
+      <div class="title">Title: {{ post.title }}</div>
+      <div>{{ post.body | snippet }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Blogs",
+  data() {
+    return {
+      posts: []
+    };
+  },
+  methods: {},
+  created() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts/")
+      .then(response => {
+        console.log("xxx posts", response.data);
+        this.posts = response.data;
+      })
+      .catch(err => console.log(err));
+  }
+};
+</script>
+
+<style scoped>
+.blog {
+  margin-bottom: 20px;
+}
+.title {
+  font-weight: bold;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
-import Vue from "vue";
-import App from "./App.vue";
+import Vue from "vue"
+import App from "./App.vue"
+
+// global filters
+Vue.filter("snippet", val => {
+  if (!val || typeof val !== "string") return ""
+  return val.slice(0, 50) + "..."
+})
 
 new Vue({
   el: "#app",
   render: h => h(App)
-});
+})
