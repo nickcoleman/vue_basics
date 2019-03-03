9eea648f6bc6357c943498cca4133e3fdc430004
Nick <nick@itprojmgt.com>
2019-03-03 13:04:21 -0700

    custom events

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+    <Navbar/>
+    <AllFriends :friends="friends" @delete="deleteFriend"/>
+    <OnlineFriends :friends="friends" @delete="deleteFriend"/>
+  </div>
+</template>
+
+<script>
+import Navbar from "./Navbar";
+import AllFriends from "./AllFriends";
+import OnlineFriends from "./OnlineFriends";
+export default {
+  name: "app",
+  components: {
+    Navbar,
+    AllFriends,
+    OnlineFriends
+  },
+  data: () => ({
+    title: "My First Vue.js App",
+    friends: [
+      { name: "Mario", online: true },
+      { name: "Luigi", online: false },
+      { name: "Toad", online: true },
+      { name: "Bowser", online: false }
+    ]
+  }),
+  methods: {
+    deleteFriend(payload) {
+      this.friends = this.friends.filter(
+        friend => friend.name !== payload.name
+      );
+    }
+  }
+};
+</script>
+
+<style>
+h1 {
+  color: #444;
+  font-weight: normal;
+  text-align: center;
+}
+</style>
---
+++ src/OnlineFriends.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="online-friends">
+    <h2>Online Friends</h2>
+    <div v-for="(friend, index) in friends" :key="index">
+      <span v-if="friend.online" @click="unfriend(friend.name)">{{ friend.name }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "OnlineFriends",
+  props: ["friends"],
+  data: () => ({}),
+  methods: {
+    unfriend(name) {
+      this.$emit("delete", { name });
+    }
+  }
+};
+</script>
+
+<style>
+</style>
